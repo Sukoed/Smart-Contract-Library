ebf7c6f1b26fbf0df2fb280e63f0ed98ca3d59ec
Sukoed <150945953+Sukoed@users.noreply.github.com>
2026-01-12 12:40:49 +0300

    Update SKD_Staking_System.sol

--- SKD_Staking_System.sol
+++ SKD_Staking_System.sol
@@ -5,8 +5,10 @@ contract SKDStaking {
     mapping(address => uint256) public stakedBalance;
     mapping(address => uint256) public lastStakedTime;
     uint256 public rewardRate = 100; // 100 units per hour
+    uint256 public totalStaked;
 
     function stake() public payable {
+        totalStaked += msg.value;
         require(msg.value > 0, "Amount must be > 0");
         stakedBalance[msg.sender] += msg.value;
         lastStakedTime[msg.sender] = block.timestamp;
