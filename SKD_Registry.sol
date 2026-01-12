699b90ee5c52a15e42af2a724f24c92a3737708b
Sukoed <150945953+Sukoed@users.noreply.github.com>
2026-01-12 12:28:31 +0300

    Create SKD_Registry.sol

---
+++ SKD_Registry.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract SKDRegistry {
+    mapping(address => string) public names;
+    function register(string memory _name) public {
+        names[msg.sender] = _name;
+    }
+}
