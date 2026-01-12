03e9f9de6019c97a51e4e9553108935ea5104c4a
Sukoed <150945953+Sukoed@users.noreply.github.com>
2026-01-12 12:30:29 +0300

    Create SKD_Staking_System.sol

---
+++ SKD_Staking_System.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract SKDStaking {
+    mapping(address => uint256) public stakedBalance;
+    mapping(address => uint256) public lastStakedTime;
+    uint256 public rewardRate = 100; // 100 units per hour
+
+    function stake() public payable {
+        require(msg.value > 0, "Amount must be > 0");
+        stakedBalance[msg.sender] += msg.value;
+        lastStakedTime[msg.sender] = block.timestamp;
+    }
+
+    function calculateReward(address _user) public view returns (uint256) {
+        uint256 timeStaked = block.timestamp - lastStakedTime[_user];
+        return (stakedBalance[_user] * rewardRate * timeStaked) / 3600;
+    }
+}
